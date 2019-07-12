1b8059808e0bb0a86d046ab3eb9c83c33fcc8a87
Jamshid93 <hamzayevjf@gmail.com>
2019-07-12 19:23:16 +0500

    Intager valuable in  Python

---
+++ intager.py
@@ -0,0 +1,6 @@
+# we use "int" variable only for natural numbers, for instance: 1,2,3,4,5,6,7,8,9,... int = intager
+number1=int (input(" "))
+number2=int(input(" "))
+number3=int()
+number3=number1+number2
+print(number3)
